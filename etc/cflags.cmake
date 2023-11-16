6fdfeb731af0899c9d273609ffd47544055c1847
Alex Lopez <agl.alexglopez@gmail.com>
2023-11-16 01:50:57 -0800

    add compile commands rather than set

--- etc/cflags.cmake
+++ etc/cflags.cmake
@@ -1,20 +1,24 @@
 set (CMAKE_C_STANDARD 11)
-set(CMAKE_C_STANDARD_REQUIRED ON)
 set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
-# set(SANITIZING_FLAGS -fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
-
-# ask for more warnings from the compiler
-set (CMAKE_BASE_C_FLAGS "${CMAKE_C_FLAGS}")
-
-# -Werror=effc++ has been removed ?
 
 # AppleClang specific flags
 if ("${CMAKE_C_COMPILER_ID}" STREQUAL "AppleClang")
-    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie")
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        # Not sure if the address sanitizers and whatnot do anything, not getting any warnings.
+        add_compile_options(-g3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE -fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
+        add_link_options(-fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
+    else()
+        add_compile_options(-O3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE -fno-sanitize-recover=all)
+    endif()
 endif()
 
 # GCC or Clang specific flags
 if ("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang" OR "${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
-    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE")
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        # Not sure if the address sanitizers and whatnot do anything, not getting any warnings.
+        add_compile_options(-g3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE -fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
+        add_link_options(-fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
+    else()
+        add_compile_options(-O3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE)
+    endif()
 endif()
