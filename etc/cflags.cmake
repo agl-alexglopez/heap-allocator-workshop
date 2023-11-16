2675cde1e71b2ed286d7e1e11e320adbdb0108d0
Alex Lopez <agl.alexglopez@gmail.com>
2023-11-16 02:36:36 -0800

    left -O3 flag in all modes by mistake

--- etc/cflags.cmake
+++ etc/cflags.cmake
@@ -5,19 +5,16 @@ set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
 if ("${CMAKE_C_COMPILER_ID}" STREQUAL "AppleClang")
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
         # Not sure if the address sanitizers and whatnot do anything, not getting any warnings.
-        add_compile_options(-g3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE -fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
-        add_link_options(-fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
+        add_compile_options(-g3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE)
     else()
-        add_compile_options(-O3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE -fno-sanitize-recover=all)
+        add_compile_options(-O3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE)
     endif()
 endif()
 
 # GCC or Clang specific flags
 if ("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang" OR "${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        # Not sure if the address sanitizers and whatnot do anything, not getting any warnings.
-        add_compile_options(-g3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE -fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
-        add_link_options(-fno-sanitize-recover=all -fsanitize=undefined -fsanitize=address)
+        add_compile_options(-g3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE)
     else()
         add_compile_options(-O3 -Wall -Wextra -Wfloat-equal -Wtype-limits -Wpointer-arith -Wshadow -Winit-self -fno-diagnostics-show-option -fcf-protection=none -fno-pic -fpie -D_GNU_SOURCE)
     endif()
