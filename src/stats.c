edcc64c85e535595fdf8493aeb4aa8cfd3a24b30
Alex Lopez <agl.alexglopez@gmail.com>
2023-12-01 14:25:36 -0800

    add command thread queue to perf

--- src/stats.c
+++ src/stats.c
@@ -1,23 +1,6 @@
-/// Author: Alexander G. Lopez
-/// Files: time_harness.c
-/// ---------------------
-/// Reads and interprets text-based script files containing a sequence of
-/// allocator requests. Runs the allocator on a script and times the requested
-/// sequence of requests from that script. Times user requested sequences of lines in the
-/// script if requested.
-/// When you compile using `make`, it will create different
-/// compiled versions of this program, one using each type of
-/// heap allocator.
-/// Most safety measures deleted for speed. Helps view the runtime efficiency in the correct
-/// time complexity without O(N) measures clouding accurate timing between calls. Do not use this
-/// unless you know your allocator is correct. Please see test_harness.c if you want to see an
-/// allocator handler more focussed on correctness.
-
 #include "allocator.h"
-#include "print_utility.h"
 #include "script.h"
 #include "segment.h"
-#include <assert.h>
 // NOLINTNEXTLINE(*include-cleaner)
 #include <getopt.h>
 #include <stdbool.h>
@@ -26,8 +9,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
-/// TYPE DECLARATION
 
 // Create targeted scripts with intervals you want to time, no point in too many requests.
 enum
@@ -56,24 +37,17 @@ const int base_10 = 10;
 /// FUNCTION PROTOTYPE
 
 static int time_script( char *script_name, struct interval_reqs *user_requests );
-static size_t time_allocator( struct script *s, struct interval_reqs *user_requests, struct gnuplots *graphs );
-static void report_interval_averages( struct interval_reqs *user_requests );
+static size_t time_allocator( struct script *s, struct interval_reqs *user_requests );
 static void validate_intervals( struct script *s, struct interval_reqs *user_requests );
-
-/// TIME EVALUATION IMPLEMENTATIO
-
-// NOLINTBEGIN(*include-cleaner)
 
 /// @brief main  parses command line arguments that request a range of lines to be timed for
 ///              performance. Arguments may take the following form:
 ///              ../bin/time_rbtree_clrs -s 10001 -e 15000 -s 15001 scripts/time-insertdelete-5k.script
-/// @arg -q      only output a single number in decimal form representing the time in ms to complete the requests.
 ///              Must be the first flag of all arguments.
 /// @arg -s      the flag to start the timer on a certain line number. May be followed by -e flag.
 ///              If no -e flag follows, the program will time the remainder of lines to execute.
 /// @arg -e      the flag to end the timer on a certain line number. Invalid if not preceeded by -s.
-/// @warning     time intervals may not overlap and with no arguments the entire program execution
-///              will be timed.
+/// @warning     time intervals may not overlap and with no arguments the entire program execution will be timed.
 int main( int argc, char *argv[] )
 {
     struct interval_reqs user_req = { 0 };
@@ -123,50 +97,18 @@ int main( int argc, char *argv[] )
     return time_script( argv[optind], &user_req );
 }
 
-// NOLINTEND(*include-cleaner)
-
 static int time_script( char *script_name, struct interval_reqs *user_requests )
 {
     struct script s = parse_script( script_name );
     validate_intervals( &s, user_requests );
-
-    // We will do some graphing with helpful info at the end of program execution.
-    struct gnuplots graphs
-        = { .util_percents = NULL, .free_nodes = NULL, .request_times = NULL, .num_ops = s.num_ops };
-    graphs.free_nodes = malloc( sizeof( size_t ) * s.num_ops );
-    assert( graphs.free_nodes );
-    graphs.util_percents = malloc( sizeof( double ) * s.num_ops );
-    assert( graphs.util_percents );
-    graphs.request_times = malloc( sizeof( double ) * s.num_ops );
-    assert( graphs.request_times );
-
-    // Evaluate this script and record the results
-    if ( !user_requests->quiet ) {
-        printf( "\nEvaluating allocator on %s...\n", s.name );
-    }
-    // We will bring back useful utilization info while we time.
-    size_t used_segment = time_allocator( &s, user_requests, &graphs );
-    if ( !user_requests->quiet ) {
-        printf(
-            "...successfully serviced %d requests. (payload/segment = %zu/%zu)\n",
-            s.num_ops,
-            s.peak_size,
-            used_segment
-        );
-        printf( "Utilization averaged %.2lf%%\n", ( 100.0 * (double)s.peak_size ) / (double)used_segment );
-        print_gnuplots( &graphs );
-        report_interval_averages( user_requests );
-    }
-
-    free( graphs.util_percents );
-    free( graphs.free_nodes );
-    free( graphs.request_times );
+    size_t used_segment = time_allocator( &s, user_requests );
+    printf( "%.2lf%%\n", ( 100.0 * (double)s.peak_size ) / (double)used_segment );
     free( s.ops );
     free( s.blocks );
     return 0;
 }
 
-static size_t time_allocator( struct script *s, struct interval_reqs *user_requests, struct gnuplots *graphs )
+static size_t time_allocator( struct script *s, struct interval_reqs *user_requests )
 {
     init_heap_segment( heap_size );
     if ( !myinit( heap_segment_start(), heap_segment_size() ) ) {
@@ -191,47 +133,17 @@ static size_t time_allocator( struct script *s, struct interval_reqs *user_reque
                 // A helpful utility function fromt the script.h lib helps time one request.
                 double request_time = time_request( s, req, &cur_size, &heap_end );
                 total_request_time += request_time;
-
-                graphs->request_times[req] = request_time;
-                graphs->free_nodes[req] = get_free_total();
-                graphs->util_percents[req] = ( 100.0 * (double)s->peak_size )
-                                             / (double)( (uint8_t *)heap_end - (uint8_t *)heap_segment_start() );
-            }
-            if ( user_requests->quiet ) {
-                printf( "%f\n", total_request_time );
-            } else {
-                printf(
-                    "Execution time for script lines %d-%d (milliseconds): %f\n",
-                    sect.start_req + 1,
-                    sect.end_req + 1,
-                    total_request_time
-                );
             }
             user_requests->interval_averages[current_interval]
                 = total_request_time / (double)( sect.end_req - sect.start_req );
+            printf( "%f %f\n", total_request_time, user_requests->interval_averages[current_interval] );
             current_interval++;
         } else {
-            double request_time = time_request( s, req, &cur_size, &heap_end );
-            graphs->request_times[req] = request_time;
-            graphs->free_nodes[req] = get_free_total();
-            graphs->util_percents[req] = ( 100.0 * (double)s->peak_size )
-                                         / (double)( (uint8_t *)heap_end - (uint8_t *)heap_segment_start() );
+            (void)exec_request( s, req, &cur_size, &heap_end );
             req++;
         }
     }
     return (uint8_t *)heap_end - (uint8_t *)heap_segment_start();
-}
-
-static void report_interval_averages( struct interval_reqs *user_requests )
-{
-    for ( size_t i = 0; i < user_requests->num_intervals; i++ ) {
-        printf(
-            "Average time (milliseconds) per request lines %d-%d: %lf\n",
-            user_requests->intervals[i].start_req + 1,
-            user_requests->intervals[i].end_req + 1,
-            user_requests->interval_averages[i]
-        );
-    }
 }
 
 static void validate_intervals( struct script *s, struct interval_reqs *user_requests )
--- tests/perf.cc
+++ tests/perf.cc
@@ -1,15 +1,20 @@
 #include <array>
 #include <cerrno>
+#include <condition_variable>
 #include <csignal>
 #include <cstdlib>
 #include <fcntl.h>
 #include <filesystem>
+#include <functional>
 #include <iostream>
+#include <mutex>
+#include <optional>
+#include <queue>
 #include <string>
 #include <string_view>
 #include <sys/wait.h>
+#include <thread>
 #include <unistd.h>
-#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -21,78 +26,124 @@ constexpr std::string_view prog_path = "/build/rel";
 constexpr std::string_view prog_path = "/build/deb";
 #endif
 
+struct process_result
+{
+    int process_id;
+    ssize_t bytes_read;
+};
+
 struct path_bin
 {
     std::string path;
     std::string bin;
 };
 
-struct runtime_per_n
+struct big_o_metrics
 {
-    size_t n;
-    double runtime;
+    std::vector<std::pair<std::string, std::vector<std::pair<size_t, double>>>> interval_speed;
+    std::vector<std::pair<std::string, std::vector<std::pair<size_t, double>>>> average_response_time;
+    std::vector<std::pair<std::string, std::vector<std::pair<size_t, double>>>> overall_utilization;
 };
 
-struct allocator_big_o
+struct allocator_entry
 {
-    std::string allocator;
-    std::vector<runtime_per_n> stats;
+    size_t index;
+    size_t script_size;
 };
+
+constexpr size_t worker_limit = 4;
 
 /// These are the commands that focus in on the key lines of the malloc free scripts to time.
-constexpr std::array<std::array<std::string_view, 6>, 20> increasing_malloc_free_size_args = { {
-    { "-q", "-s", "10001", "-e", "20000", "scripts/time-insertdelete-05k.script" },
-    { "-q", "-s", "20001", "-e", "40000", "scripts/time-insertdelete-10k.script" },
-    { "-q", "-s", "30001", "-e", "60000", "scripts/time-insertdelete-15k.script" },
-    { "-q", "-s", "40001", "-e", "80000", "scripts/time-insertdelete-20k.script" },
-    { "-q", "-s", "50001", "-e", "100000", "scripts/time-insertdelete-25k.script" },
-    { "-q", "-s", "60001", "-e", "120000", "scripts/time-insertdelete-30k.script" },
-    { "-q", "-s", "70001", "-e", "140000", "scripts/time-insertdelete-35k.script" },
-    { "-q", "-s", "80001", "-e", "160000", "scripts/time-insertdelete-40k.script" },
-    { "-q", "-s", "90001", "-e", "180000", "scripts/time-insertdelete-45k.script" },
-    { "-q", "-s", "100001", "-e", "200000", "scripts/time-insertdelete-50k.script" },
-    { "-q", "-s", "110001", "-e", "220000", "scripts/time-insertdelete-55k.script" },
-    { "-q", "-s", "120001", "-e", "240000", "scripts/time-insertdelete-60k.script" },
-    { "-q", "-s", "130001", "-e", "260000", "scripts/time-insertdelete-65k.script" },
-    { "-q", "-s", "140001", "-e", "280000", "scripts/time-insertdelete-70k.script" },
-    { "-q", "-s", "150001", "-e", "300000", "scripts/time-insertdelete-75k.script" },
-    { "-q", "-s", "160001", "-e", "320000", "scripts/time-insertdelete-80k.script" },
-    { "-q", "-s", "170001", "-e", "340000", "scripts/time-insertdelete-85k.script" },
-    { "-q", "-s", "180001", "-e", "360000", "scripts/time-insertdelete-90k.script" },
-    { "-q", "-s", "190001", "-e", "380000", "scripts/time-insertdelete-95k.script" },
-    { "-q", "-s", "200001", "-e", "400000", "scripts/time-insertdelete-100k.script" },
+constexpr std::array<std::array<std::string_view, 5>, 20> increasing_malloc_free_size_args = { {
+    { "-s", "10001", "-e", "20000", "scripts/time-insertdelete-05k.script" },
+    { "-s", "20001", "-e", "40000", "scripts/time-insertdelete-10k.script" },
+    { "-s", "30001", "-e", "60000", "scripts/time-insertdelete-15k.script" },
+    { "-s", "40001", "-e", "80000", "scripts/time-insertdelete-20k.script" },
+    { "-s", "50001", "-e", "100000", "scripts/time-insertdelete-25k.script" },
+    { "-s", "60001", "-e", "120000", "scripts/time-insertdelete-30k.script" },
+    { "-s", "70001", "-e", "140000", "scripts/time-insertdelete-35k.script" },
+    { "-s", "80001", "-e", "160000", "scripts/time-insertdelete-40k.script" },
+    { "-s", "90001", "-e", "180000", "scripts/time-insertdelete-45k.script" },
+    { "-s", "100001", "-e", "200000", "scripts/time-insertdelete-50k.script" },
+    { "-s", "110001", "-e", "220000", "scripts/time-insertdelete-55k.script" },
+    { "-s", "120001", "-e", "240000", "scripts/time-insertdelete-60k.script" },
+    { "-s", "130001", "-e", "260000", "scripts/time-insertdelete-65k.script" },
+    { "-s", "140001", "-e", "280000", "scripts/time-insertdelete-70k.script" },
+    { "-s", "150001", "-e", "300000", "scripts/time-insertdelete-75k.script" },
+    { "-s", "160001", "-e", "320000", "scripts/time-insertdelete-80k.script" },
+    { "-s", "170001", "-e", "340000", "scripts/time-insertdelete-85k.script" },
+    { "-s", "180001", "-e", "360000", "scripts/time-insertdelete-90k.script" },
+    { "-s", "190001", "-e", "380000", "scripts/time-insertdelete-95k.script" },
+    { "-s", "200001", "-e", "400000", "scripts/time-insertdelete-100k.script" },
 } };
 
-constexpr std::array<std::array<std::string_view, 6>, 20> increasing_realloc_free_size_args = { {
-    { "-q", "-s", "15001", "-e", "20000", "scripts/time-reallocfree-05k.script" },
-    { "-q", "-s", "30001", "-e", "40000", "scripts/time-reallocfree-10k.script" },
-    { "-q", "-s", "45001", "-e", "60000", "scripts/time-reallocfree-15k.script" },
-    { "-q", "-s", "60001", "-e", "80000", "scripts/time-reallocfree-20k.script" },
-    { "-q", "-s", "75001", "-e", "100000", "scripts/time-reallocfree-25k.script" },
-    { "-q", "-s", "90001", "-e", "120000", "scripts/time-reallocfree-30k.script" },
-    { "-q", "-s", "105001", "-e", "140000", "scripts/time-reallocfree-35k.script" },
-    { "-q", "-s", "120001", "-e", "160000", "scripts/time-reallocfree-40k.script" },
-    { "-q", "-s", "135001", "-e", "180000", "scripts/time-reallocfree-45k.script" },
-    { "-q", "-s", "150001", "-e", "200000", "scripts/time-reallocfree-50k.script" },
-    { "-q", "-s", "165001", "-e", "220000", "scripts/time-reallocfree-55k.script" },
-    { "-q", "-s", "180001", "-e", "240000", "scripts/time-reallocfree-60k.script" },
-    { "-q", "-s", "195001", "-e", "260000", "scripts/time-reallocfree-65k.script" },
-    { "-q", "-s", "210001", "-e", "280000", "scripts/time-reallocfree-70k.script" },
-    { "-q", "-s", "225001", "-e", "300000", "scripts/time-reallocfree-75k.script" },
-    { "-q", "-s", "240001", "-e", "320000", "scripts/time-reallocfree-80k.script" },
-    { "-q", "-s", "255001", "-e", "340000", "scripts/time-reallocfree-85k.script" },
-    { "-q", "-s", "270001", "-e", "360000", "scripts/time-reallocfree-90k.script" },
-    { "-q", "-s", "285001", "-e", "380000", "scripts/time-reallocfree-95k.script" },
-    { "-q", "-s", "300001", "-e", "400000", "scripts/time-reallocfree-100k.script" },
+constexpr std::array<std::array<std::string_view, 5>, 20> increasing_realloc_free_size_args = { {
+    { "-s", "15001", "-e", "20000", "scripts/time-reallocfree-05k.script" },
+    { "-s", "30001", "-e", "40000", "scripts/time-reallocfree-10k.script" },
+    { "-s", "45001", "-e", "60000", "scripts/time-reallocfree-15k.script" },
+    { "-s", "60001", "-e", "80000", "scripts/time-reallocfree-20k.script" },
+    { "-s", "75001", "-e", "100000", "scripts/time-reallocfree-25k.script" },
+    { "-s", "90001", "-e", "120000", "scripts/time-reallocfree-30k.script" },
+    { "-s", "105001", "-e", "140000", "scripts/time-reallocfree-35k.script" },
+    { "-s", "120001", "-e", "160000", "scripts/time-reallocfree-40k.script" },
+    { "-s", "135001", "-e", "180000", "scripts/time-reallocfree-45k.script" },
+    { "-s", "150001", "-e", "200000", "scripts/time-reallocfree-50k.script" },
+    { "-s", "165001", "-e", "220000", "scripts/time-reallocfree-55k.script" },
+    { "-s", "180001", "-e", "240000", "scripts/time-reallocfree-60k.script" },
+    { "-s", "195001", "-e", "260000", "scripts/time-reallocfree-65k.script" },
+    { "-s", "210001", "-e", "280000", "scripts/time-reallocfree-70k.script" },
+    { "-s", "225001", "-e", "300000", "scripts/time-reallocfree-75k.script" },
+    { "-s", "240001", "-e", "320000", "scripts/time-reallocfree-80k.script" },
+    { "-s", "255001", "-e", "340000", "scripts/time-reallocfree-85k.script" },
+    { "-s", "270001", "-e", "360000", "scripts/time-reallocfree-90k.script" },
+    { "-s", "285001", "-e", "380000", "scripts/time-reallocfree-95k.script" },
+    { "-s", "300001", "-e", "400000", "scripts/time-reallocfree-100k.script" },
 } };
 
-size_t parse_quantity_n( std::string_view script_name )
-{
-    const size_t last_dash = script_name.find_last_of( '-' ) + 1;
-    const size_t last_k = script_name.find_last_of( 'k' );
-    const std::string num = std::string( script_name.substr( last_dash, last_k - last_dash ) );
-    return std::stoull( num ) * 1000;
-}
+class command_queue {
+    std::queue<std::optional<std::function<void()>>> q_;
+    std::mutex lk_;
+    std::condition_variable wait_;
+    std::vector<std::thread> workers_;
+    void start()
+    {
+        for ( ;; ) {
+            std::unique_lock<std::mutex> ul( lk_ );
+            wait_.wait( ul, [this]() -> bool { return !q_.empty(); } );
+            auto new_task = std::move( q_.front() );
+            q_.pop();
+            ul.unlock();
+            if ( !new_task.has_value() ) {
+                return;
+            }
+            new_task.value()();
+        }
+    }
+
+  public:
+    explicit command_queue( size_t num_workers )
+    {
+        for ( size_t i = 0; i < num_workers; ++i ) {
+            workers_.emplace_back( [this]() { start(); } );
+        }
+    }
+    ~command_queue()
+    {
+        for ( auto &w : workers_ ) {
+            w.join();
+        }
+    }
+    void push( std::optional<std::function<void()>> args )
+    {
+        std::unique_lock<std::mutex> ul( lk_ );
+        q_.push( std::move( args ) );
+        wait_.notify_one();
+    }
+    command_queue( const command_queue & ) = delete;
+    command_queue &operator=( const command_queue & ) = delete;
+    command_queue( command_queue && ) = delete;
+    command_queue &operator=( command_queue && ) = delete;
+};
 
 std::vector<path_bin> gather_timer_programs()
 {
@@ -103,14 +154,49 @@ std::vector<path_bin> gather_timer_programs()
         const std::string as_str = entry.path();
         const size_t last_entry = as_str.find_last_of( '/' );
         const std::string_view is_timer = std::string_view{ as_str }.substr( last_entry + 1 );
-        if ( is_timer.starts_with( "time_" ) ) {
+        if ( is_timer.starts_with( "stats_" ) ) {
             commands.emplace_back( as_str, std::string( is_timer ) );
         }
     }
     return commands;
 }
+void close_process( process_result res )
+{
+    close( res.process_id );
+    int err = 0;
+    waitpid( -1, &err, 0 );
+    if ( WIFSIGNALED( err ) && WTERMSIG( err ) == SIGSEGV ) { // NOLINT
+        std::cerr << "seg fault\n";
+        std::abort();
+    }
+    if ( res.bytes_read == -1 ) {
+        std::cerr << "read error\n";
+        std::abort();
+    }
+}
 
-int script_timer_process( const path_bin &cmd, const std::array<std::string_view, 6> &args )
+size_t parse_quantity_n( std::string_view script_name )
+{
+    const size_t last_dash = script_name.find_last_of( '-' ) + 1;
+    const size_t last_k = script_name.find_last_of( 'k' );
+    const std::string num = std::string( script_name.substr( last_dash, last_k - last_dash ) );
+    return std::stoull( num ) * 1000;
+}
+
+void parse_metrics( const std::string &output, const allocator_entry &entry, big_o_metrics &m )
+{
+    const size_t first_space = output.find_first_of( ' ' );
+    const size_t first_newline = output.find_first_of( '\n' );
+    const std::string interval_time = output.substr( 0, first_space );
+    m.interval_speed[entry.index].second.emplace_back( entry.script_size, stod( interval_time ) );
+    const std::string response_average = output.substr( first_space, first_newline - first_space );
+    m.average_response_time[entry.index].second.emplace_back( entry.script_size, stod( response_average ) );
+    const std::string utilization
+        = output.substr( first_newline + 1, output.find_last_of( '%' ) - ( first_newline + 1 ) );
+    m.overall_utilization[entry.index].second.emplace_back( entry.script_size, stod( utilization ) );
+}
+
+int allocator_stats_subprocess( std::string_view cmd_path, const std::array<std::string_view, 6> &args )
 {
     std::array<int, 2> comms{ 0, 0 };
     if ( pipe2( comms.data(), O_CLOEXEC ) < 0 ) {
@@ -126,55 +212,75 @@ int script_timer_process( const path_bin &cmd, const std::array<std::string_view
         std::cerr << "child cannot communicate to parent.\n";
         std::abort();
     }
-    std::string cur_path = std::filesystem::current_path();
-    cur_path.append( "/" );
-    std::array<std::string, 7> local_copy = {
-        std::string( cmd.bin ),
-        std::string( args[0] ),
-        std::string( args[1] ),
-        std::string( args[2] ),
-        std::string( args[3] ),
-        std::string( args[4] ),
-        cur_path + std::string( args[5] ),
-    };
-    std::array<char *, 8> arg_copy{};
-    for ( size_t i = 0; i < local_copy.size(); ++i ) {
-        arg_copy.at( i ) = local_copy.at( i ).data();
+    std::array<const char *, 7> arg_copy{};
+    for ( size_t i = 0; i < args.size(); ++i ) {
+        arg_copy.at( i ) = args.at( i ).data();
     }
     arg_copy.back() = nullptr;
-    execv( cmd.path.data(), static_cast<char *const *>( arg_copy.data() ) ); // NOLINT
+    execv( cmd_path.data(), const_cast<char *const *>( arg_copy.data() ) ); // NOLINT
     std::cerr << "child process failed abnormally errno: " << errno << "\n";
     std::abort();
 }
 
-std::unordered_map<std::string, std::vector<std::pair<size_t, double>>>
-time_malloc_frees( const std::vector<path_bin> &commands )
+/// The work we do to gather timing is trivially parallelizable. We just need a parent to
+/// monitor this small stat generation program and enter the results. So we can have
+/// threads become the parents for these parallel processes and they will just add the
+/// stats to the big_o_metrics container that has preallocated space for them. All good.
+void thread_fill_data( const size_t allocator_index, const path_bin &cmd, big_o_metrics &m )
 {
-    std::unordered_map<std::string, std::vector<std::pair<size_t, double>>> times{};
-    for ( const auto &c : commands ) {
-        auto allocator_entry = times.insert( { c.bin.substr( c.bin.find( '_' ) + 1 ), {} } );
-        for ( const auto &args : increasing_malloc_free_size_args ) {
-            const int timer_process = script_timer_process( c, args );
-            std::vector<char> vec_buf( 50 );
-            ssize_t bytes_read = 0;
-            while ( ( bytes_read = read( timer_process, vec_buf.data(), 50 ) ) > 0 ) {}
-            close( timer_process );
-            int err = 0;
-            waitpid( -1, &err, 0 );
-            if ( WIFSIGNALED( err ) && WTERMSIG( err ) == SIGSEGV ) { // NOLINT
-                std::cerr << "seg fault\n";
-                std::abort();
+    const std::string title = cmd.bin.substr( cmd.bin.find( '_' ) + 1 );
+    for ( const auto &args : increasing_malloc_free_size_args ) {
+        const size_t script_size = parse_quantity_n( args.back() );
+        std::string cur_path = std::filesystem::current_path();
+        cur_path.append( "/" ).append( std::string( args[4] ) );
+        const int process = allocator_stats_subprocess(
+            cmd.path,
+            {
+                cmd.bin,
+                args[0],
+                args[1],
+                args[2],
+                args[3],
+                cur_path,
             }
-            if ( bytes_read == -1 ) {
-                std::cerr << "read error\n";
-                std::abort();
-            }
-            std::string buf( vec_buf.data() );
-            buf = buf.substr( 0, buf.find_first_of( '\n' ) );
-            allocator_entry.first->second.emplace_back( parse_quantity_n( args.back() ), stod( buf ) );
+        );
+        std::vector<char> vec_buf( 64 );
+        ssize_t bytes_read = 0;
+        size_t progress = 0;
+        while ( ( bytes_read = read( process, &vec_buf.at( progress ), 64 ) ) > 0 ) {
+            progress += bytes_read;
         }
+        close_process( { process, bytes_read } );
+        std::string data = std::string( vec_buf.data() );
+        parse_metrics(
+            data,
+            allocator_entry{
+                allocator_index,
+                script_size,
+            },
+            m
+        );
     }
-    return times;
+}
+
+void time_malloc_frees( const std::vector<path_bin> &commands, big_o_metrics &m )
+{
+    for ( const auto &c : commands ) {
+        const std::string title = c.bin.substr( c.bin.find( '_' ) + 1 );
+        m.interval_speed.push_back( { title, {} } );
+        m.interval_speed.back().second.reserve( increasing_malloc_free_size_args.size() );
+        m.average_response_time.push_back( { title, {} } );
+        m.average_response_time.back().second.reserve( increasing_malloc_free_size_args.size() );
+        m.overall_utilization.push_back( { title, {} } );
+        m.overall_utilization.back().second.reserve( increasing_malloc_free_size_args.size() );
+    }
+    command_queue workers( worker_limit );
+    for ( size_t i = 0; i < commands.size(); ++i ) {
+        workers.push( [i, &commands, &m]() { thread_fill_data( i, commands[i], m ); } );
+    }
+    for ( size_t i = 0; i < worker_limit; ++i ) {
+        workers.push( {} );
+    }
 }
 
 } // namespace
@@ -182,11 +288,11 @@ time_malloc_frees( const std::vector<path_bin> &commands )
 int main()
 {
     const std::vector<path_bin> commands = gather_timer_programs();
-    const std::unordered_map<std::string, std::vector<std::pair<size_t, double>>> times
-        = time_malloc_frees( commands );
-    for ( const auto &t : times ) {
-        std::cout << t.first << ":\n";
-        for ( const std::pair<size_t, double> &stat : t.second ) {
+    big_o_metrics m{};
+    time_malloc_frees( commands, m );
+    for ( const auto &metric : m.interval_speed ) {
+        std::cout << metric.first << ":\n";
+        for ( const std::pair<size_t, double> &stat : metric.second ) {
             std::cout << stat.first << " " << stat.second << "\n";
         }
     }
